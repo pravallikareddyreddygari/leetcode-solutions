6148a42ff14f32f2feded26d0ca3223fd11bde2c
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-10 09:54:35 +0530

    Added addDigit leetcode solution

---
+++ addDigits.js
@@ -0,0 +1,21 @@
+var getSumAndProductOfDigits = function (n) {
+    let sumOfDigits = 0;
+    let productOfDigits = 1;
+    while (n) {
+        let rem = n % 10;
+        sumOfDigits += rem;
+        productOfDigits *= rem;
+        n = Math.trunc(n / 10);
+    }
+    return { sumOfDigits, productOfDigits };
+};
+
+var addDigits = function (num) {
+    // while 2 or more digits
+    while (num >= 10) {
+        let { sumOfDigits } = getSumAndProductOfDigits(num)
+        num = sumOfDigits
+    }
+
+    return num // 1 digit
+};

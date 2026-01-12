9eaa564cc37357f64b9771f7ab3d1304cf20655b
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-12 09:39:32 +0530

    palindrome solution

---
+++ 9.palindrome.js
@@ -0,0 +1,9 @@
+var isPalindrome = function (x) {
+    let num = String(x);
+    let rev = num.split("").reverse().join("");
+    if (num === rev) {
+        return true;
+    } else {
+        return false;
+    }
+};

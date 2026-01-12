92d07052b399f3f7112f59ec623a3a4a94457809
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-12 09:54:07 +0530

    updated corrrect variable

--- 9.palindrome.js
+++ 9.palindrome.js
@@ -1,6 +1,6 @@
 var isPalindrome = function (x) {
-    let num = String(x);
-    let rev = num.split("").reverse().join("");
+    let num = String(x)
+    let rev = num.split("").reverse().join("")
     if (num === rev)  return true
     else return false;
 };

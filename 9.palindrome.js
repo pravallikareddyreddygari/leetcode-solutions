ff174e734303ddac4692fb5b2e7f52b6465b9dd7
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-12 09:52:41 +0530

    changed to short code

--- 9.palindrome.js
+++ 9.palindrome.js
@@ -1,9 +1,6 @@
 var isPalindrome = function (x) {
     let num = String(x);
     let rev = num.split("").reverse().join("");
-    if (num === rev) {
-        return true;
-    } else {
-        return false;
-    }
+    if (num === rev)  return true
+    else return false;
 };

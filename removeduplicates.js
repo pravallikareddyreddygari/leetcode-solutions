3c4c50e5c33884932da37b1baa9336b24bc091d0
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-14 21:32:13 +0530

    commited leet code solution

---
+++ removeduplicates.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(a) {
+    for (let i = 0; i < a.length; i++) { // go next
+        if (a[i] === a[i + 1]) {
+            a.splice(i + 1, 1) // delete duplicate
+            i-- 
+        }
+    }
+    return a.length
+};

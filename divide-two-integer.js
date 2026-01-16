d4bf1fce25493d8db417f30bc469f307596a613a
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-16 20:15:33 +0530

    changes committed

--- divide-two-integer.js
+++ divide-two-integer.js
@@ -1,5 +1,10 @@
 var divide = function (dividend, divisor) {
     let res = dividend / divisor;
-    if (res > 2 ** 31 - 1) return 2 ** 31 - 1;
-    else return Math.trunc(res);
+    if (res > 2 ** 31 - 1){
+return 2 ** 31 - 1;
+    }
+    else{
+         return Math.trunc(res);
+    }
+    
 };

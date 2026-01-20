89d1036a206899f398c512832a1277d911a91089
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-20 16:32:38 +0530

    leetcode sol

--- removeduplicatessortedlist.js
+++ removeduplicatessortedlist.js
@@ -1,8 +1,7 @@
 var deleteDuplicates = function (head) {
     let current = head
-    
+
     while (current !== null && current.next !== null) {
-        
         if (current.val === current.next.val) {
             current.next = current.next.next
         } else {

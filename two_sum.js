b03203ed1b6c22aac17f07c4cd8c8af075ed1879
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-21 15:42:03 +0530

    changed

--- two_sum.js
+++ two_sum.js
@@ -1,9 +1,10 @@
 var twoSum = function(nums, target) {
     for(let i=0;i<nums.length;i++){
         for(let j=i+1;j<nums.length;j++){
-            sum=nums[i]+nums[j]
+           let sum=nums[i]+nums[j]
+
             if(target==sum){
-              return [i,j]
+             return [i,j]
             }
         }
     }

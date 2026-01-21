c8b5233dfc70c81f8970efb548b08a26a9a6fd08
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-21 15:42:13 +0530

    commit

--- removeduplicatessortedlist.js
+++ removeduplicatessortedlist.js
@@ -3,6 +3,7 @@ var deleteDuplicates = function (head) {
 
     while (current !== null && current.next !== null) {
         if (current.val === current.next.val) {
+            
             current.next = current.next.next
         } else {
             current = current.next

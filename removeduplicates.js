4868a5bc2aaa0bf885fba172cad20acdf5e4c389
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-01-25 08:33:42 +0530

    commit

--- removeduplicates.js
+++ removeduplicates.js
@@ -1,5 +1,6 @@
 var removeDuplicates = function(a) {
     for (let i = 0; i < a.length; i++) { // go next
+        
         if (a[i] === a[i + 1]) {
             a.splice(i + 1, 1) // delete duplicate
             i-- 

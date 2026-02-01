75267fae03fec2c28a264cc5ad4675ac6db14525
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-01 10:14:30 +0530

    commit

--- smallest-range-I.js
+++ smallest-range-I.js
@@ -1,8 +1,9 @@
 var smallestRangeI = function (a, k) {
-    
+
     a.sort((a, b) => a - b)
     let first = a[0]
     let last = a.at(-1)
-
-    return (last - k) - (first + k) < 0 ? 0 : (last - k) - (first + k)
+    let diff = (last - k) - (first + k)
+    
+    return  diff < 0 ? 0 : diff
 };

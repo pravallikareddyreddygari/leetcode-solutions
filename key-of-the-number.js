95d606ba7ef9b86f6a75bd4848069d6e38654406
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-02 10:30:23 +0530

    commit

--- key-of-the-number.js
+++ key-of-the-number.js
@@ -7,6 +7,7 @@ var generateKey = function (num1, num2, num3) {
     let s3 = getFourLen0PaddedStr(num3)
     let sMins = ''
 
+    
     for (let i = 0; i < 4; i++) {
         sMins += Math.min(+s1[i], +s2[i], +s3[i])
     }

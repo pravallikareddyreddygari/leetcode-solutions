b5ee539f0409f826b167b2bd30727a3292e1c4dc
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-03 10:43:04 +0530

    count largest grp leet code solution

---
+++ count-largest-group.js
@@ -0,0 +1,32 @@
+var getSumAndProductOfDigits = function (n) {
+    let sumOfDigits = 0;
+    let productOfDigits = 1;
+    while (n) {
+        let rem = n % 10;
+        sumOfDigits += rem;
+        productOfDigits *= rem;
+        n = Math.trunc(n / 10);
+    }
+    return { sumOfDigits, productOfDigits };
+};
+
+const countLargestGroup = function (n) {
+    const freq = {}
+    let maxFreq = 0
+
+    for (let i = 1; i <= n; i++) {
+        let { sumOfDigits } = getSumAndProductOfDigits(i)
+        freq[sumOfDigits] = (freq[sumOfDigits] || 0) + 1
+
+        maxFreq = Math.max(maxFreq, freq[sumOfDigits])
+    }
+
+    let maxFreqCount = 0
+    for (const f of Object.values(freq)) {
+        if (f === maxFreq) {
+            maxFreqCount++
+        }
+    }
+
+    return maxFreqCount
+}

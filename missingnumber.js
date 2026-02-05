0ca5424dcd9e03bf1d0e93e45d9ced4e72239a13
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-05 11:10:16 +0530

    commit

---
+++ missingnumber.js
@@ -0,0 +1,9 @@
+var missingNumber = function (nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!nums.includes(i)) {
+            return i;
+        }
+    }
+    return nums.length;
+};

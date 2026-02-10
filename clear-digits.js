20493e1996bf229a385ae0685059b070e340b33a
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-10 09:42:28 +0530

    clear digit leetcode solution

--- clear-digits.js
+++ clear-digits.js
@@ -3,7 +3,16 @@ var clearDigits = function (s) {
     const top = (stack) => stack.at(-1)
 
     let maxd = 0
-
+    for (let ch of s) {
+        if (/[a-z]/.test(ch)) {
+            stack.push(ch) // add alphabet
+        } else {
+            // number
+            if (/[a-z]/.test(top(stack))) {
+                stack.pop() // remove alphabet
+            } else {
+                stack.push(ch) // add number
+            }
    
         }
     }

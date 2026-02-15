5f20e12d1ff0644ea5103bc3ddafc8a20a4a247f
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-15 08:51:35 +0530

    commit

---
+++ min-no.of-smallest-and-largest-number.js
@@ -0,0 +1,17 @@
+var minimumAverage = function (nums) {
+    let avg = []
+    let res = []
+    let p = [...nums].sort((a, b) => a - b)
+    for (let i = 0; i < p.length; i++) {
+        while (p.length > 0) {
+            let minimumValue = p.shift()
+            //cl(minimumValue)
+            let maximumValue = p.pop()
+            //cl(maximumValue)
+            let average = (minimumValue + maximumValue) / 2
+            // cl(average)
+            avg.push(average)
+        }
+    }
+    return Math.min(...avg)
+};

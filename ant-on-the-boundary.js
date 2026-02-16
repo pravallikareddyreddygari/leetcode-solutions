e8a81f3fb68c7062e3817a68acf0f3d2ef27452b
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-16 17:26:58 +0530

    commit

--- ant-on-the-boundary.js
+++ ant-on-the-boundary.js
@@ -2,7 +2,7 @@ var returnToBoundaryCount = function (nums) {
 
     let currentPos= 0
     let boundaryCross = 0
-    or (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         currentPos += nums[i]
         if (currentPos=== 0) {
             boundaryCross++

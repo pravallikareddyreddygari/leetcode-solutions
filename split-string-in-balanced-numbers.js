d6d5e000e8aebdaa33bdaf2a666aada12bd28696
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-18 10:46:44 +0530

    commit

--- split-string-in-balanced-numbers.js
+++ split-string-in-balanced-numbers.js
@@ -2,7 +2,6 @@ var balancedStringSplit = function (s) {
     let balancedCount = 0
     let left = 0
     let right = 0
-    
     for (let ch of s) {
         if (ch === 'L') left++
         if (ch === 'R') right++

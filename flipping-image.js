4fac386495d9ee3a368cf1c21befca943b9ba43f
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-02-27 09:46:48 +0530

    commit

--- flipping-image.js
+++ flipping-image.js
@@ -1,7 +1,6 @@
 var flipAndInvertImage = function (image) {
     let m = image.length // r
     let n = image[0].length // c
-
     for (let row of image) {
         row.reverse()
         for (let j = 0; j < n; j++) {

2b23689ca85ee28d1e1dbda2763fe223563fe838
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-06 10:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (27.14%) - LeetSync

---
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.js
@@ -0,0 +1,15 @@
+var squareIsWhite = function (s) {
+    let [ch, num] = s;
+    num = +num
+    let num2 = ch.charCodeAt() - 97;
+    return (num + num2) % 2 === 0
+}
+
+var squareIsWhite22222 = function (s) {
+    // s = "a1"
+    const [ch, num] = s;
+    const cc = ch.charCodeAt() - 97;
+    const parityOfChar = (cc + 1) % 2; //   for a1, its 1
+    const parityOfNum = num % 2; //         for a1, its 1
+    return parityOfChar !== parityOfNum; // for a1, its false, is black
+};

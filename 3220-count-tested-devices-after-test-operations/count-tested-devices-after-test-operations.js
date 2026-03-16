1022dee47c444e496c4d32bc9ab4367352790bea
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-16 15:00:32 +0530

    Time: 1 ms (60.61%) | Memory: 54.5 MB (100.00%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.js
@@ -0,0 +1,9 @@
+var countTestedDevices = function (batteryPercentage) {
+    let testedCount = 0
+    for (let i = 0; i < batteryPercentage.length; i++) {
+        if (batteryPercentage[i] > testedCount) {
+            testedCount++
+        }
+    }
+    return testedCount
+};

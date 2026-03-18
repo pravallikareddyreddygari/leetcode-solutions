3b90384ac54ec54e642c946b0d338e4d2324aad7
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-18 19:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (76.84%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,9 @@
+var mostWordsFound = function(sentences) {
+    let res = 0;
+    for (let i=0;i<sentences.length;i++) {
+        let  words= sentences[i].split(" ");
+        res = Math.max(words.length,res);
+    }
+    return res;
+};
+   

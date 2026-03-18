bcb2ee3288a5ac5937c3a942439d12e70ed94af6
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-18 19:25:06 +0530

    Time: 5 ms (4.82%) | Memory: 57.4 MB (9.63%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -0,0 +1,18 @@
+var scoreOfString = function (s) {
+    let Asciivalues = []
+    let res = []
+    for (let i = 0; i < s.length; i++) {
+        let values = s.charCodeAt(i)
+        Asciivalues.push(values)
+    }
+    for (let j = 0; j < Asciivalues.length - 1; j++) {
+        let newAscii = Math.abs(Asciivalues[j] - Asciivalues[j + 1])
+        res.push(newAscii)
+
+    }
+    let sum = 0
+    for (let q = 0; q < res.length; q++) {
+        sum += res[q];
+    }
+    return sum
+};

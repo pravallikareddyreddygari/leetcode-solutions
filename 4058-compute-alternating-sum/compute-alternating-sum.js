6c22e12f3ad8cfee8c0515b0929c2e7d2edc13bf
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-19 19:27:48 +0530

    Time: 1 ms (54.58%) | Memory: 57.9 MB (5.63%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.js
@@ -0,0 +1,12 @@
+const cl = console.log;
+var alternatingSum = function (nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 == 0) {
+            sum += nums[i]
+        } else {
+            sum -= nums[i]
+        }
+    }
+    return sum
+};

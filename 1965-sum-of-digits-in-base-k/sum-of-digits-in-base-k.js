650d88c6a4f31e107ceb8498592f04a4d6590a7e
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-22 21:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (5.09%) - LeetSync

---
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.js
@@ -0,0 +1,10 @@
+var sumBase = function (n, k) {
+    let sumOfDigits = 0;
+
+    while (n) {
+        sumOfDigits += n % k; // last digit, base k division
+        n = Math.trunc(n / k) // remove last digit, base k division
+    }
+
+    return sumOfDigits;
+};

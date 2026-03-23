b8c3e8c6365d84952366637d559cb9d45f5be0e9
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-23 11:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (86.35%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -1,4 +1,8 @@
 var getConcatenation = function (nums) {
-    return [...nums, ...nums]
-
-};
+    const res=[]
+    for(let i=0;i<nums.length;i++){
+        res[i]=nums[i]
+        res[i+nums.length]=nums[i]
+    }
+    return res
+}

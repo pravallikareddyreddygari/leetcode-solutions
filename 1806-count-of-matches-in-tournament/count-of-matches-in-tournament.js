496302ae0e822dd3caaea286944b587bfd2e730c
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-24 12:01:34 +0530

    Time: 48 ms (23.94%) | Memory: 52.8 MB (92.96%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -1,20 +1,9 @@
-const cl = console.log
+
 var numberOfMatches = function (n) {
-    let totalSum = 0
+    let count = 0
     while (n > 1) {
-        let Matches;
-        let teamAdvance;
-        if (n % 2 === 0) {
-            Matches = n / 2
-            teamAdvance = n / 2
-            //cl(teamsAdvance)
-        } else{
-            Matches = (n - 1) / 2
-            teamAdvance = (n - 1) / 2 + 1
-            //cl(teamAdvance)
-        }
-        totalSum += Matches
-            n = teamAdvance
+        count += Math.floor(n / 2)
+        n = Math.ceil(n / 2)
     }
-    return totalSum
-}
+    return count
+};

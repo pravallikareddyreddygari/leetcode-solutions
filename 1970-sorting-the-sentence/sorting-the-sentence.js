8b1d8052d26c4d43ecf5068a5f1fcb9ffcf0d643
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-03-28 21:59:38 +0530

    Time: 42 ms (63.39%) | Memory: 53.9 MB (31.69%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.js
+++ 1970-sorting-the-sentence/sorting-the-sentence.js
@@ -2,17 +2,12 @@ const cl = console.log
 var sortSentence = function (s) {
     let res = []
     let a = s.split(" ")
-    // cl(s)
-    // cl(a)
     for (let w of a) {
         let wl = w.length
-        // 0, wl-2     wl-1
         let first = w.slice(0, wl - 1)
         let lastIndex = +w.slice(wl - 1) - 1
-        // cl({ w, first, lastIndex })
         res[lastIndex] = first
     }
-    // cl(res)
     return res.join(" ")
 
 };

771692c9468b657a11cff5567781f7b2a8d95fa4
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-04-01 09:42:11 +0530

    Time: 1 ms (81.13%) | Memory: 56 MB (61.32%) - LeetSync

---
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.js
@@ -0,0 +1,11 @@
+var subarraySum = function (nums) {
+    let totalSum = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        let start = Math.max(0, i - nums[i])
+        for (let j = start; j <= i; j++) {
+            totalSum += nums[j]
+        }
+    }
+    return totalSum
+}

7c505fa7eab8a90c4064f33bdd9c533912673a7e
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-04-12 20:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (87.72%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.js
@@ -0,0 +1,14 @@
+var countAsterisks = function (s) {
+    let a = s.split("|")
+
+    let n = a.length
+    let total = 0
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            let ss = a[i] // e**
+            let starC = ss.length - ss.replaceAll('*', '').length
+            total += starC
+        }
+    }
+    return total
+};

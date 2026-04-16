1fd62eb6804a352dc533538887efbd7e7adbbb6c
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-04-16 21:43:30 +0530

    Time: 8 ms (13.86%) | Memory: 57.3 MB (38.61%) - LeetSync

---
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.js
@@ -0,0 +1,12 @@
+var findPermutationDifference = function (s, t) {
+    let pos = []
+    for (let i = 0; i < s.length; i++) {
+        pos[s[i]] = i;
+    }
+    let diff = 0;
+    for (let i = 0; i < t.length; i++) {
+        diff += Math.abs(pos[t[i]] - i);
+    }
+
+    return diff;
+} 

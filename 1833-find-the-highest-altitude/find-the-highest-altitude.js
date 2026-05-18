9205b8d3c9ab06937f885aaa0b30d3c578577b07
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-05-18 21:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (81.89%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.js
@@ -0,0 +1,11 @@
+var largestAltitude = function (gain) {
+    let max = 0;
+    let sum = 0
+    for (let i = 0; i < gain.length; i++) {
+        sum += gain[i];
+        if (sum > max) {
+            max = sum;
+        }
+    }
+    return max;
+}

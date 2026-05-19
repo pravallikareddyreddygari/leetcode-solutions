d7fa2acbff509e485185efef417794bd2c019e01
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-05-19 20:35:03 +0530

    commit

--- check-straight-line.js
+++ check-straight-line.js
@@ -2,7 +2,6 @@ var checkStraightLine = function (points) {
     const getSlope = ([x1, y1], [x2, y2]) => (y2 - y1) / (x2 - x1);
     let prevPoint;
     let prevSlope;
-    
     for (let point of points) {
         if (prevPoint !== undefined) {
             let slope = getSlope(prevPoint, point)

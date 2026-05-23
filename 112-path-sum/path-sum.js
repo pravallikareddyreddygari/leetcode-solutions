ce96a025529f75852a9588a463145e1533880c91
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-05-23 09:53:18 +0530

    commit

--- 112-path-sum/path-sum.js
+++ 112-path-sum/path-sum.js
@@ -6,7 +6,6 @@ var hasPathSum = function (root, targetSum) {
     if (!root.left && !root.right) {
         return root.val === targetSum;
     }
-
     return hasPathSum(root.left, targetSum - root.val) ||
         hasPathSum(root.right, targetSum - root.val)
 

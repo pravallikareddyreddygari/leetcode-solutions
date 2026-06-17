6461a1b98a4e48cda22bd2467fcc33e551d74550
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-06-17 20:50:28 +0530

    commit

--- min-abs-difference.js
+++ min-abs-difference.js
@@ -2,7 +2,7 @@ var minimumAbsDifference = function (arr) {
     arr.sort((a, b) => a - b)
     let minDiff = Infinity
     let res = []
-    
+
     for (let i = 1; i < arr.length; i++) {
         let diff = arr[i] - arr[i - 1]
         if (diff < minDiff) {
@@ -15,5 +15,6 @@ var minimumAbsDifference = function (arr) {
             res.push([arr[i - 1], arr[i]])
         }
     }
+    
     return res
 }

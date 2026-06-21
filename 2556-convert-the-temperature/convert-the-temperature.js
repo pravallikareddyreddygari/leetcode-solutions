2814343c4d6a51327133f5a1bced40a66af2f532
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-06-21 17:56:23 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (40.11%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -0,0 +1,5 @@
+var convertTemperature = function (celsius) {
+    let Kelvin = (Celsius) => Celsius + 273.15
+    let Fahrenheit = (Celsius) => Celsius * 1.80 + 32.00
+    return [Kelvin(celsius), Fahrenheit(celsius)]
+};

a6cec13ffb0184a2ee569ace512928bb57ee4258
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-06-21 17:57:07 +0530

    Time: 1 ms (45.76%) | Memory: 54.6 MB (18.08%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -2,7 +2,7 @@ var numIdenticalPairs = function (nums) {
     let count = 0
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] == nums[j] && i<j) {
+            if (nums[i] == nums[j]) {
                 count += 1
             }
         }

ec2e19ebbf2b9da428cb42912999baf2633d1def
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-07-02 08:11:38 +0530

    commit

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.js
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.js
@@ -1,9 +1,9 @@
 var evenNumberBitwiseORs = function(nums) {
     let sum=0
-for(let i=0;i<nums.length;i++){
+    for(let i=0;i<nums.length;i++){
     if(nums[i]%2==0){
         sum |= nums[i]
     }
-}
+    }
    return sum 
 };

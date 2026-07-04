4c273f709c7724e066fb1e49af3825c8bb31fe99
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-07-04 07:30:33 +0530

    Time: 77 ms (11.89%) | Memory: 90.6 MB (15.38%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.js
@@ -0,0 +1,20 @@
+var findCenter = function (edges) {
+    let E = edges.length // 3
+    let V = E + 1 // 4
+    let indegree = {}
+    let outdegree = {}
+    for (let [u, v] of edges) {
+        // u   ->   v
+        outdegree[u] = (outdegree[u] ?? 0) + 1
+        indegree[v] = (indegree[v] ?? 0) + 1
+        // u   <-   v
+        outdegree[v] = (outdegree[v] ?? 0) + 1
+        indegree[u] = (indegree[u] ?? 0) + 1
+    }
+
+    for (let u = 1; u <= V; u++) {
+        if (indegree[u] === E) {
+            return u
+        }
+    }
+};

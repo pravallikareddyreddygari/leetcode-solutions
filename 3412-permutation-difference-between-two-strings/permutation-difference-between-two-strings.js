ca5c8ac3658d240785c6b898865178272cf58de2
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-07-23 10:07:18 +0530

    commit

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.js
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.js
@@ -3,7 +3,6 @@ var findPermutationDifference = function (s, t) {
     for (let i = 0; i < s.length; i++) {
         pos[s[i]] = i;
     }
-
     let diff = 0;
 
     for (let i = 0; i < t.length; i++) {

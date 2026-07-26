dc74bfd8b4e87dc3880ad5e40a85299c721bf2ee
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-07-26 21:37:31 +0530

    commit

--- 2556-convert-the-temperature/convert-the-temperature.js
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -1,5 +1,6 @@
 var convertTemperature = function (celsius) {
     let Kelvin = (Celsius) => Celsius + 273.15
     let Fahrenheit = (Celsius) => Celsius * 1.80 + 32.00
+   
     return [Kelvin(celsius), Fahrenheit(celsius)]
 };

c4094eb61bf95fc3c12fbf826cd1cc53364af158
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-07-29 12:39:19 +0530

    commit

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -1,6 +1,5 @@
 var mostWordsFound = function(sentences) {
     let res = 0;
-    
     for (let i=0;i<sentences.length;i++) {
         let  words= sentences[i].split(" ");
         res = Math.max(words.length,res);

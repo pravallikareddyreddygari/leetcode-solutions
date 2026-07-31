2329d61b313be77b81b4309d2b19a815261b557c
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-07-31 14:32:54 +0530

    Time: 1 ms (20.51%) | Memory: 54.3 MB (23.08%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.js
@@ -0,0 +1,9 @@
+const canFormTriangle = ([x1, y1], [x2, y2], [x3, y3]) => {
+    const hasSameSlope = (y2 - y1) * (x3 - x2) === (y3 - y2) * (x2 - x1)
+    return !hasSameSlope
+}
+
+var isBoomerang = function (points) {
+    let [p1, p2, p3] = points
+    return canFormTriangle(p1, p2, p3)
+};

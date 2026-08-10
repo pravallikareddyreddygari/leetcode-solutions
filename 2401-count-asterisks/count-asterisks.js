5d59d5ad01fd6de6f261af5bd0e99fdb9675e0da
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-08-10 19:22:15 +0530

    commit

--- 2401-count-asterisks/count-asterisks.js
+++ 2401-count-asterisks/count-asterisks.js
@@ -2,6 +2,7 @@ var countAsterisks = function (s) {
     let a = s.split("|")
     let n = a.length
     let total = 0
+    
     for (let i = 0; i < n; i++) {
         if (i % 2 === 0) {
             let ss = a[i] // e**

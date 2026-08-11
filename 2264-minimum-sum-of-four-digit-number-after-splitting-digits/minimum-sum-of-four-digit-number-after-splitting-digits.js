15d6c46ae0f633843b551513e09d34558c73890a
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-08-11 21:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (76.06%) - LeetSync

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -0,0 +1,10 @@
+var minimumSum = function (num) {
+    let arr = (num + "")
+        .split("")
+        .map((x) => +x)
+        .sort((a, b) => a - b);
+
+    let new1 = arr[0] + "" + arr[2];
+    let new2 = arr[1] + "" + arr[3];
+    return +new1 + +new2;
+};

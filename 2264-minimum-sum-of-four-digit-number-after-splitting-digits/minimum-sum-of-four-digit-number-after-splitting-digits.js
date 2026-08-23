de44c0918e3c9a39b0a8f5da64e9cafc847e94a5
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-08-23 21:11:10 +0530

    commit

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -3,7 +3,6 @@ var minimumSum = function (num) {
         .split("")
         .map((x) => +x)
         .sort((a, b) => a - b);
-
     let new1 = arr[0] + "" + arr[2];
     let new2 = arr[1] + "" + arr[3];
     return +new1 + +new2;

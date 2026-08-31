18d4afb202090607767361357c14bab99569dac8
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-08-31 09:40:12 +0530

    commit

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.js
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.js
@@ -1,5 +1,6 @@
 var sumBase = function (n, k) {
     let sumOfDigits = 0;
+    
     while (n) {
         sumOfDigits += n % k; // last digit, base k division
         n = Math.trunc(n / k) // remove last digit, base k division

1228cb6531d446c339b1786bcaac8addafbbec66
Pravallika Reddy Reddygari <reddygaripravallikareddy@gmail.com>
2026-09-01 11:06:06 +0530

    Time: 3 ms (33.94%) | Memory: 56.5 MB (42.20%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.js
@@ -0,0 +1,20 @@
+var clearDigits = function (s) {
+    let stack = []
+    const top = (stack) => stack.at(-1)
+
+    let maxd = 0
+
+    for (let ch of s) {
+        if (/[a-z]/.test(ch)) {
+            stack.push(ch) // add alphabet
+        } else {
+            // number
+            if (/[a-z]/.test(top(stack))) {
+                stack.pop() // remove alphabet
+            } else {
+                stack.push(ch) // add number
+            }
+        }
+    }
+    return stack.join("")
+};
